74d2c232763e1bebbb3d35941f58bb4de6b038b9
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2015-08-29 17:58:07 +0300

    CommandLineParserTest.bat added to solution

---
+++ CommandLineParserTest.bat
@@ -0,0 +1,8 @@
+CommandLineParser
+CommandLineParser -help /?
+CommandLineParser /help -print abc def "aaa ttt" -k a 1 b 2 c -ooo /? -d
+CommandLineParser -print abc def "aaa ttt" -k a 1 b 2 c -ping -print -ooo
+CommandLineParser -help -print p1 23456 -k a 1 b 2 c -ping -print p2 abcdef -k O o -print -d -k -ooo /?
+CommandLineParser GGGG
+CommandLineParser Uuuu -t oOOOO -d dd/MM/yyyy /help
+pause
