35a02a19e532f11c04e01ae8642bd26c7cc4d7ed
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2015-08-29 17:23:28 +0300

    Complete project

---
+++ CommandLineParser/CommandParser.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    class Program
+    {
+        static bool HelpPrinted = false;
+        static void Main(string[] args)
+        {
+            string[] defaultArguments = { "/?" };
+            if (args.Length == 0)
+                ParseArguments(defaultArguments);
+            else
+                ParseArguments(args);
+        }
+
+        static void ParseArguments(String[] args)
+        {
+            String command="";
+            Queue<String> arguments = new Queue<String>();
+
+            foreach (String str in args)
+            {
+                if (str[0] == '-' || str[0] == '/')
+                { // assume command
+                    // execute previous command
+                    ExecuteCommand(command, arguments);
+                    // initialize new command
+                    command = str;
+                    arguments.Clear();
+                }
+                else
+                { // assume argument for command
+                    arguments.Enqueue(str);
+                }
+            }
+            ExecuteCommand(command, arguments);
+        }
+        static void ExecuteCommand(String command, Queue<String> arguments)
+        {
+            if (command == "" && arguments.Count == 0L)
+                return;
+            switch (command.ToLower())
+            {
+                case "/?":
+                case "/help":
+                case "-help":
+                    if (!HelpPrinted)
+                    {
+                        Console.WriteLine("{0} [/?] [/help] [-help] [-k key value] [-ping] [-print <print a value>] [-d [date-format]]", System.IO.Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location));
+                        HelpPrinted = true;
+                    }
+                    break;
+                case "-k":
+                    CommandKeyValue(arguments);
+                    break;
+                case "-d":
+                    CommandDate(arguments);
+                    break;
+                case "-ping":
+                    CommandPing(); // arguments ignored
+                    break;
+                case "-print":
+                    CommandPrint(arguments);
+                    break;
+                case "":
+                    Console.WriteLine("No command for given arguments, use {0} /? to see set of allowed commands",
+                        System.IO.Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location)
+                        );
+                    break;
+                default:
+                    Console.WriteLine("Command {0} is not supported, use {1} /? to see set of allowed commands", 
+                        command, 
+                        System.IO.Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location)
+                        );
+                    break;
+            }
+        }
+        static void CommandKeyValue(Queue<String> arguments)
+        {
+            while (arguments.Count > 0)
+            {
+                String key = arguments.Dequeue();
+                String value = (arguments.Count > 0) ? arguments.Dequeue() : "<null>";
+                Console.WriteLine("{0} - {1}", key, value);
+            }
+        }
+        static void CommandPing()
+        {
+            Console.WriteLine("Pinging...");
+            Console.Beep();
+        }
+        static void CommandPrint(Queue<String> arguments)
+        {
+            if (arguments.Count > 0)
+                Console.WriteLine(String.Join(" ",arguments));
+        }
+        static void CommandDate(Queue<String> arguments)
+        {
+            String format = (arguments.Count > 0) ? arguments.Dequeue() : "yyyy-MM-dd";
+            Console.WriteLine(DateTime.Today.ToString(format));
+        }
+    
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# C# Command Line Parser
+Command Line Parser is a test application submitted to [Kottans](http://kottans.org/) to apply for a C# course.
+The application parses console command line arguments producing predictable results according to an assignment.
+
+## Requirements
+To build an app you need Visual Studio 2010+
+
+## Features
+Command line arguments
+* `-k key value key value...` - prints pairs of key and value; `<null>` replaces absent value
+* `-print message to print` - prints given message
+* `-ping` - beeps and prints "Pinging..." NB! No actual pinging is effected whatever it might mean except the behaviour described
+* `-d [dateFormat]` - prints current date using given format; default format is yyyy-MM-dd
+Use `/?`, `-help` or `/help` command-line argument to get help from the application in console.
+
+## Compiltaion
+Open solution with Visual Studio and tap Play button.
+
+## Tests
+1. Launch Windows command prompt (Menu -> Run program -> cmd.exe)
+2. Go to solution folder (cd path-to-solution-directory).
+3. Go to application executable folder (cd CommandLineParser/bin/Debug).
+4. Launch CommandLineParserTest.bat
+You will see app behaviour run with various command-line arguments as well as error handling.
+
+## Usage
+Well, I doubt one can make any real use from this application. But, please, feel free playing around.
+CommandLineParserTest.bat is a good source of ideas for that.
+
+## Author
+Oleksiy Rudenko oleksiy.rudenko@gmail.com
