eea32e81812f354248d33e51ea6d1b19d4ce9f0a
Oleksiy Rudenko <orudenko@bigmir.net>
2015-09-16 21:36:14 +0300

    Added class CmdPing (extends Cmd) - implements [-ping] command.

---
+++ CommandLineParser/CmdPing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    class CmdPing : Cmd
+    {
+        public static String Usage()
+        {
+            return "-ping";
+        }
+        public String ToString()
+        {
+            Console.Beep();
+            return "Pinging...";
+        }
+    }
+}
