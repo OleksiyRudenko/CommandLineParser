337e923314835803ee969cb3d6c51caeef790508
Oleksiy Rudenko <orudenko@bigmir.net>
2015-09-16 21:33:15 +0300

    Added class CmdD (extends Cmd) - implements [-d [date-format]] command.

---
+++ CommandLineParser/CmdD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    class CmdD : Cmd
+    {
+        public static String Usage()
+        {
+            return "-k key value";
+        }
+        public String ToString()
+        {
+            String format = (arguments.Count > 0) ? arguments.Dequeue() : "yyyy-MM-dd";
+            return DateTime.Today.ToString(format);
+        }
+    }
+}
