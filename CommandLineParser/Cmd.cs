06c077b19818edeabba7e1bb10db6be70fc5dbf9
Oleksiy Rudenko <orudenko@bigmir.net>
2015-09-16 21:18:27 +0300

    Added new classes: Cmd - abstract class to implement commands; CmdK (extends Cmd) - implements [-k key value] command

---
+++ CommandLineParser/Cmd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    abstract class Cmd
+    {
+        protected Queue<String> arguments = new Queue<String>();
+
+        public Cmd(Queue<String> arguments)
+        {
+            arguments = this.arguments;
+        }
+
+        // abstract void Execute();
+
+        abstract static String Usage();
+    }
+}
---
+++ CommandLineParser/CmdK.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    class CmdK : Cmd
+    {
+        public static String Usage()
+        {
+            return "-k key value";
+        }
+        public String ToString()
+        {
+            StringBuilder result = new StringBuilder("");
+            while (arguments.Count > 0)
+            {
+                String key = arguments.Dequeue();
+                String value = (arguments.Count > 0) ? arguments.Dequeue() : "<null>";
+                result.AppendFormat("{0} - {1}{2}", key, value, Environment.NewLine);
+            }
+            return result.ToString();
+        }
+    }
+}
