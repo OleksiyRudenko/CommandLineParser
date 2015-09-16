ba033addcc35d4623e8b1b353f2cbc9300e5f0d9
Oleksiy Rudenko <orudenko@bigmir.net>
2015-09-16 21:40:03 +0300

    Added class CmdPrint (extends Cmd) - implements [-print <value to print>] command.

---
+++ CommandLineParser/CmdPrint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    class CmdPrint : Cmd
+    {
+        public static String Usage()
+        {
+            return "-print <value to print>";
+        }
+        public String ToString()
+        {
+            return (arguments.Count > 0) 
+                ?
+                String.Join(" ",arguments)
+                :
+                "";
+        }
+    }
+}
