dcd57bd4fd76a37879ed6fdb9783fc8f59023a2d
Oleksiy Rudenko <orudenko@bigmir.net>
2015-09-17 14:07:51 +0300

    CommandLineParserTest.bat amended to show new features.

--- CommandLineParserTest.bat
+++ CommandLineParserTest.bat
@@ -1,10 +1,12 @@
 @cd CommandLineParser/bin/Debug
 CommandLineParser
-CommandLineParser -help /?
-CommandLineParser /help -print abc def "aaa ttt" -k a 1 b 2 c -ooo /? -d
-CommandLineParser -print abc def "aaa ttt" -k a 1 b 2 c -ping -print -ooo
-CommandLineParser -help -print p1 23456 -k a 1 b 2 c -ping -print p2 abcdef -k O o -print -d -k -ooo /?
+CommandLineParser -help /? -exit
+CommandLineParser /help -print abc def "aaa ttt" -k a 1 b 2 c -ooo /? -d -exit
+CommandLineParser -print abc def "aaa ttt" -k a 1 b 2 c -ping -print -ooo -exit
+CommandLineParser -help -print p1 23456 -k a 1 b 2 c -ping -print p2 abcdef -k O o -print -d -k -ooo /? -exit
 CommandLineParser GGGG
-CommandLineParser Uuuu -t oOOOO -d dd/MM/yyyy /help
+CommandLineParser Uuuu -t oOOOO -d dd/MM/yyyy /help -exit
+CommandLineParser -print Test exit. Date shouldn't appear -exit -d
+CommandLineParser -print Try running commands from console
 @cd ../../..
 @pause
