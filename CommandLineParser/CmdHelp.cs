714c34451cb50dbad001c7d726d3f45b10687a15
Oleksiy Rudenko <orudenko@bigmir.net>
2015-09-17 12:58:41 +0300

    Added class CmdHelp (extends Cmd) - implements [-help] command.

---
+++ CommandLineParser/CmdHelp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser
+{
+    class CmdHelp : Cmd
+    {
+        override public String Usage()
+        {
+            return "/?] [/help] [-help";
+        }
+        override public String ToString()
+        {
+            List<String> result = new List<String>();
+
+            return System.IO.Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location) + " "
+                + String.Join("] [",result)
+                + ']';
+        }
+    }
+}
